e4eb5e7323f04f2210159009c3ddd31b626a3c59
Filbert Phang <fillberth@gmail.com>
2023-03-18 03:42:35 +0800

    Misc: Update SampleDataUtil to prevent unchecked casts

--- src/main/java/seedu/recipe/model/util/SampleDataUtil.java
+++ src/main/java/seedu/recipe/model/util/SampleDataUtil.java
@@ -12,7 +12,6 @@ import seedu.recipe.model.tag.Tag;
 
 import java.util.Arrays;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
@@ -20,31 +19,38 @@ import java.util.stream.Collectors;
  */
 public class SampleDataUtil {
     public static Recipe[] getSampleRecipes() {
-        Recipe sample1 = new Recipe(new Name("Chicken and Broccoli Stir-Fry"));
+        Recipe sample1 = new Recipe(new Name("Chicken and Broccoli Stir Fry"));
         sample1.setDuration(RecipeDuration.of("15 minutes"));
-        sample1.setTags(createArrayFromItemNames(Tag::new, "Chinese", "Healthy"));
-        sample1.setIngredients(createArrayFromItemNames(Ingredient::new, "Chicken Breast", "Broccoli", "Soy Sauce",
-                                                        "Vegetable Oil", "Garlic", "Bell Pepper"));
-        sample1.setSteps(createArrayFromItemNames(Step::new, "1 In a small bowl, mix soy sauce and crushed garlic.",
-                                                  "2 In a wok, heat vegetable oil over high heat.",
-                                                  "3 Add chicken and stir-fry for 2-3 minutes, until browned.",
-                                                  "4 Add broccoli and stir-fry for another 2-3 minutes.",
-                                                  "5 Pour the sauce over the chicken and vegetables and stir-fry for another 2-3 minutes, until the sauce thickens and coats everything evenly.",
-                                                  "6 Serve."
-                                                 ));
+        sample1.setTags(new Tag("Chinese"),
+                        new Tag("Healthy"));
+        sample1.setIngredients(new Ingredient("Chicken Breast"),
+                               new Ingredient("Broccoli"),
+                               new Ingredient("Soy Sauce"),
+                               new Ingredient("Vegetable Oil"),
+                               new Ingredient("Garlic"),
+                               new Ingredient("Bell Pepper"));
+        sample1.setSteps(new Step("In a small bowl, mix soy sauce and crushed garlic."),
+                         new Step("In a wok, heat vegetable oil over high heat."),
+                         new Step("Add chicken and stir-fry for 2-3 minutes, until browned."),
+                         new Step("Add broccoli and stir-fry for another 2-3 minutes."),
+                         new Step("Pour the sauce over the chicken and vegetables and stir-fry for " +
+                                          "another 2-3 minutes, until the sauce thickens and coats everything evenly."),
+                         new Step("Serve.)"));
 
         Recipe sample2 = new Recipe(new Name("Balsamic Glazed Chicken"));
-        sample1.setPortion(RecipePortion.of("1 - 2 Portions"));
-        sample1.setDuration(RecipeDuration.of("40 minutes"));
-        sample1.setTags(createArrayFromItemNames(Tag::new, "Gluten-free", "Healthy"));
-        sample1.setIngredients(createArrayFromItemNames(Ingredient::new, "Chicken Breast", "Balsamic Vinegar",
-                                                        "Honey"));
-        sample1.setSteps(createArrayFromItemNames(Step::new, "1 Preheat oven to 375 F.",
-                                                  "2 In a small bowl, mix together balsamic vinegar and honey.",
-                                                  "3 Arrange chicken breasts in a baking dish and pour the balsamic vinegar over them.",
-                                                  "4 Bake for 25-30 minutes, until the chicken is cooked through and the glaze is thick and bubbly.",
-                                                  "5 Serve hot."
-                                                 ));
+        sample2.setPortion(RecipePortion.of("1 - 2 Portions"));
+        sample2.setTags(new Tag("GlutenFree"), new Tag("Healthy"));
+        sample2.setIngredients(new Ingredient("Chicken Breast"),
+                               new Ingredient("Balsamic Vinegar"),
+                               new Ingredient("Honey"));
+        sample2.setSteps(
+                new Step("Preheat oven to 375 F."),
+                new Step("In a small bowl, mix together balsamic vinegar and honey."),
+                new Step("Arrange chicken breasts in a baking dish and pour the balsamic vinegar over them."),
+                new Step("Bake for 25-30 minutes, until the chicken is cooked through " +
+                                 "and the glaze is thick and bubbly."),
+                new Step("Serve hot.)"
+                ));
 
         Recipe sample3 = new Recipe(new Name("Cacio E Pepe Pasta"));
         sample3.setDuration(RecipeDuration.of("15 minutes"));
@@ -75,13 +81,4 @@ public class SampleDataUtil {
     public static Set<Tag> getTagSet(String... strings) {
         return Arrays.stream(strings).map(Tag::new).collect(Collectors.toSet());
     }
-
-    // generates an array of items (Tag[], Ingredients[], Step[], ...)
-    // from a list of Strings (the item names)
-    // assumption: generator function only needs their name to create an item
-    public static <T> T[] createArrayFromItemNames(Function<String, T> generator, String... items) {
-        // probably safe enough
-        return (T[]) Arrays.stream(items).map(generator).toArray();
-    }
-
 }
